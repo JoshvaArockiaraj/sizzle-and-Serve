207cc9ea8d9c999807d16541920f5538114ba5a9
Joshva Arockiaraj <JoshvaArockiaraj@users.noreply.github.com>
2024-09-16 12:44:30 +0530

    Js and Css files optimized

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const currentYear = new Date().getFullYear()
-document.getElementById('dynamicyear').textContent=currentYear
+document.addEventListener('DOMContentLoaded',function(){
+    const currentYear = new Date().getFullYear()
+    document.getElementById('dynamicyear').textContent=currentYear
+    
+    document.getElementById('toggleMenu').addEventListener('click',toggleMenu)
+
+    function toggleMenu(){
+        const navList = document.getElementById('nav-list');
+        navList.classList.toggle('show')
+    }
+})
+
